6e135ce2f19b0a03266f6fb2d75e68d6fc60bcde
ishineee <80478582+ishineee@users.noreply.github.com>
2021-12-04 20:26:42 +0100

    Update update_or_reinstall.py

--- update_or_reinstall.py
+++ update_or_reinstall.py
@@ -1,24 +1,24 @@
 import requests
 from time import sleep
-from bs4 import BeautifulSoup
-import os
 
 print("LOGS:")
 print("[1] Opening the newest version from github...")
-url = "https://raw.githubusercontent.com/ishineee/youtube-dwnloader/main/Youtube_Downloader.py?token=ATGAC5WOYQWHXAWV52CYEQTBVJ25S"
+url = "https://raw.githubusercontent.com/ishineee/youtube-dwnloader/main/Youtube_Downloader.py?token=ATGAC5VZFKBTT4EXBYDPPOLBVO7PO"
 print("Done.")
 print("[2] Checking if Youtube_Downloader.py exists...")
-directory = os.getcwd()
 r = requests.get(url)
-
 f = open("Youtube_Downloader.py",'w')
+print("Done.")
+print("[3] checking the type of the raw file...")
 print(type(r.content))
+print("Done.")
+print("[4] Decoding the code...")
 dowolnie = r.content
 dowolnie = dowolnie.decode("utf-8")
-f.write(dowolnie)
 print("Done.")
-print("[3] Starting the protocol...")
-
+print("[5] Writing decoded code...")
+f.write(dowolnie)
+sleep(3)
 print("Done.")
 print("Newest version has been downloaded. Have fun!\n Closing Program in 2 seconds.")
 sleep(2)
